1aecf7db270f7841ba56804999270b4fb889e4c5
Yaseen Alkhafaji <yaseena@google.com>
2021-07-14 21:00:28 +0000

    add protolock testdata import/export to bazel
    Signed-off-by: Yaseen Alkhafaji <yaseena@google.com>

--- tools/api_protolock/protolock_test.py
+++ tools/api_protolock/protolock_test.py
@@ -7,19 +7,25 @@ import subprocess
 
 DEFAULT_PROTOLOCK_ARGS = ["--plugins="]
 
+
 class ProtolockTests(unittest.TestCase):
+
     def setUp(self):
         # create a temporary directory to house the proto.lock file and current proto file
         self.temp_dir = tempfile.TemporaryDirectory()
-    
+
     def tearDown(self):
         self.temp_dir.cleanup()
-    
+
     def run_protolock_test(self, testtype, testname, additional_args=None, expect_no_changes=False):
         # there's probably a better/safer way to navigate
         # but it seems like check_spelling_pedantic_test.py takes this approach as well
-        tests_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "testdata", "api_protolock", testtype)
-        protolock_args = DEFAULT_PROTOLOCK_ARGS + [f"--protoroot={self.temp_dir.name}", f"--lockdir={self.temp_dir.name}"]
+        tests_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "..", "testdata", "api_protolock",
+            testtype)
+        protolock_args = DEFAULT_PROTOLOCK_ARGS + [
+            f"--protoroot={self.temp_dir.name}", f"--lockdir={self.temp_dir.name}"
+        ]
         if additional_args is not None:
             protolock_args.extend(additional_args)
 
@@ -43,13 +49,13 @@ class ProtolockTests(unittest.TestCase):
 
         self.assertEqual(len(initial_result.stdout), 0)
         self.assertEqual(len(initial_result.stderr), 0)
-        
+
         lock_location = os.path.join(self.temp_dir.name, "proto.lock")
         with open(lock_location) as f:
             initial_lock = f.readlines()
 
         copyfile(changed, target)
-        
+
         final_result = subprocess.run(["protolock", "commit", *protolock_args], capture_output=True)
         with open(lock_location) as f:
             post_lock = f.readlines()
@@ -69,28 +75,32 @@ class ProtolockTests(unittest.TestCase):
         else:
             raise ValueError(f"Unknown test type: {testtype}")
 
+
 class TestBreakingChanges(ProtolockTests):
+
     def test_change_field_id(self):
         self.run_protolock_test("breaking", self.test_change_field_id.__name__)
-    
+
     def test_change_field_type(self):
         self.run_protolock_test("breaking", self.test_change_field_type.__name__)
 
     def test_change_field_plurality(self):
         self.run_protolock_test("breaking", self.test_change_field_plurality.__name__)
-    
+
     def test_change_field_name(self):
         self.run_protolock_test("breaking", self.test_change_field_name.__name__)
 
     @unittest.skip("package name detection not yet added to Protolock (simply requires plugin)")
     def test_change_package_name(self):
         self.run_protolock_test("breaking", self.test_change_package_name.__name__)
-    
-    @unittest.skip("oneof support not yet added to Protolock (requires Protolock changes AND plugin)")
+
+    @unittest.skip(
+        "oneof support not yet added to Protolock (requires Protolock changes AND plugin)")
     def test_change_field_from_oneof(self):
         self.run_protolock_test("breaking", self.test_change_field_from_oneof.__name__)
 
-    @unittest.skip("oneof support not yet added to Protolock (requires Protolock changes AND plugin)")
+    @unittest.skip(
+        "oneof support not yet added to Protolock (requires Protolock changes AND plugin)")
     def test_change_field_to_oneof(self):
         self.run_protolock_test("breaking", self.test_change_field_to_oneof.__name__)
 
@@ -102,11 +112,15 @@ class TestBreakingChanges(ProtolockTests):
     def test_change_pgv_message(self):
         self.run_protolock_test("breaking", self.test_change_pgv_message.__name__)
 
-    @unittest.skip("PGV oneof option support not yet added to Protolock (requires Protolock changes AND plugin)")
+    @unittest.skip(
+        "PGV oneof option support not yet added to Protolock (requires Protolock changes AND plugin)"
+    )
     def test_change_pgv_oneof(self):
         self.run_protolock_test("breaking", self.test_change_pgv_oneof.__name__)
 
+
 class TestAllowedChanges(ProtolockTests):
+
     def test_add_comment(self):
         self.run_protolock_test("allowed", self.test_add_comment.__name__, expect_no_changes=True)
 
@@ -124,6 +138,7 @@ class TestAllowedChanges(ProtolockTests):
 
     def test_force_breaking_change(self):
         self.run_protolock_test("allowed", self.test_force_breaking_change.__name__, ["--force"])
+
 
 if __name__ == '__main__':
     unittest.main()
